5aef2204b41313ad3fde4c6030e83d9437874e55
Javier Jimenez Mendoza <x4vyjm@gmail.com>
2022-11-14 10:34:35 -0500

    fix

--- dataAPP/config.py
+++ dataAPP/config.py
@@ -3,11 +3,20 @@ class ConfigAPP():
     def __init__(self):
         self.ticks = [
                     "BNBBTC",
-                    "ETHBTC"
+                    "ETHBTC",
+                    "SOLBTC",
+                    "AVAXBTC",
+                    "ADABTC",
+                    "DOTBTC",
+                    "LINKBTC",
+                    "MATICBTC",
+                    "UNIBTC",
+                    "ALGOBTC",
+                    "ATOMBTC"
                 ]
-        self.timeframe = "5m"
+        self.timeframe = "15m"
 
-        self.start = "01-01-2022"
+        self.start = "01-01-2017"
 
         self.end = "30-12-2022"
-        self.indicators = ['rsi', 'adx', 'macd', 'ema50', 'ema9', 'ema21', 'ema100', 'ema200', 'change', 'cambio', 'obv']
+        self.indicators = ['rsi', 'adx', 'macd', 'macd_hist', 'macd_signal', 'ema50', 'ema9', 'ema21', 'ema100', 'ema200', 'change', 'cambio', 'obv', ]
