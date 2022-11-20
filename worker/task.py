01c873f7eeb7c3151c51e00b75c33cb0bbba7c89
Javier Jimenez Mendoza <x4vyjm@gmail.com>
2022-11-19 21:10:58 -0500

    fixes print

--- worker/task.py
+++ worker/task.py
@@ -48,28 +48,28 @@ def runTrader(self,bot_config):
         logger.info(f"### the bot status to RUNNING was changed")
         while True:            
             aux_timestamp = conn.getLastNTimestamp(bot_config["pairs"][0], bot_config["timeframe"], 1)[0]
-            print(f"aux_timestamp loaded: {aux_timestamp}")
+            #print(f"aux_timestamp loaded: {aux_timestamp}")
             # only if aux_timestamp is diferent we can make prediction
             if(last_timestamp != aux_timestamp):
                 logger.info("### there is a new timestamp")
                 # Get the last candles in every pair from databases
                 lastPairsCandle = [conn.getLastCandleByOpenTime(item,bot_config["timeframe"], last_timestamp) for item in bot_config["pairs"]]
-                print(f"last_pairsCandle: {type(lastPairsCandle)}, {lastPairsCandle} ")
+                #print(f"last_pairsCandle: {type(lastPairsCandle)}, {lastPairsCandle} ")
                 #logger.info(mlmodel.columns)
                 # Convert above data to dataframe and add column pair
-                print(f"mlmodel.columns: {mlmodel.columns} type: {type(mlmodel.columns)}")
-                print(f"aux_columns: {aux_columns} type: {type(aux_columns)}")
+                #print(f"mlmodel.columns: {mlmodel.columns} type: {type(mlmodel.columns)}")
+                #print(f"aux_columns: {aux_columns} type: {type(aux_columns)}")
                 data_to_df = datasEntity(lastPairsCandle,aux_columns)
-                print("data to df: ",data_to_df)
+                #print("data to df: ",data_to_df)
                 #logger.info(data_to_df)
                 #logger.info(type(data_to_df))
                 df = pd.DataFrame(data_to_df)
-                print(f"data_to_df converted to df: {df}")
+                #print(f"data_to_df converted to df: {df}")
                 df["pair"] = bot_config["pairs"]
                 print("column pair added to df")
                 # send dataset to predict function and receive a list of dicts with "pairs" and "buy" keys
                 predicted_dict = mlmodel.predict(df) # return dict
-                logger.info(f"### the model prediction is: {str(predicted_dict)}")                
+                #logger.info(f"### the model prediction is: {str(predicted_dict)}")                
                 # send the pairs and signal dataframe to strategy
                 #it has the format [{"pair": "ETHBTC", "buy": false, "close": 0.001}, {"pair": "DOTBTC", "buy": false, "close": 0.001}, {"pair": "SOLBTC", "buy": false, "close": 0.001}]
                 logger.info("### the prediction was send to strategy")
