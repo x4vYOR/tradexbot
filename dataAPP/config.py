e821649b294d6ddf0893a092e5efa73e8c350798
Javier Jimenez Mendoza <x4vyjm@gmail.com>
2022-11-21 21:06:09 -0500

    pre train online

--- dataAPP/config.py
+++ dataAPP/config.py
@@ -19,4 +19,4 @@ class ConfigAPP():
         self.start = "01-01-2017"
 
         self.end = "30-12-2022"
-        self.indicators = ['rsi', 'adx', 'macd', 'macd_hist', 'macd_signal', 'ema50', 'ema9', 'ema21', 'ema100', 'ema200', 'change', 'cambio', 'obv', ]
+        self.indicators = ['rsi', 'adx', 'macd', 'macd_hist', 'macd_signal', 'ema50', 'ema9', 'ema21', 'ema100', 'ema200', 'change', 'cambio', 'obv']
