8167b50a54596892acdf53e345350e3fc5afaac7
jjimenezm1901 <jjimenezm@inside.com.pe>
2025-02-16 14:11:23 -0500

    fix pairs

--- dataAPP/config.py
+++ dataAPP/config.py
@@ -7,17 +7,12 @@ class ConfigAPP():
                     "AVAXUSDT",
                     "ADAUSDT",
                     "ALGOUSDT",
-                    "DOTUSDT",
                     "LINKUSDT",
-                    "UNIUSDT",
-                    "MANAUSDT",
-                    "THETAUSDT",
-                    "BTCUSDT",
-                    "VETUSDT"
+                    "BTCUSDT"
                 ]
         self.timeframe = "30m"
 
-        self.start = "01-01-2020"
+        self.start = "01-01-2017"
 
-        self.end = "30-12-2024"
+        self.end = "30-12-2025"
         self.indicators = ['rsi', 'adx', 'macd', 'macd_hist', 'macd_signal', 'ema50', 'ema9', 'ema21', 'ema100', 'ema200', 'change', 'cambio', 'obv','mfi','cci','mom','atr','bbands']
